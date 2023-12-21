e7fee62dfe1d0e95d25a127dc05a03c8d6089d07
S3nsu1k4n <saschakoeppe@gmail.com>
2023-12-21 21:09:48 +0900

    add navbar + login

--- app/views.py
+++ app/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
 from .models import Post
 # Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
   posts = Post.objects.all()
+  
 
   context = {
     'posts': posts,
     'posts_count': posts.count(),
   }
+
+  if request.user.is_authenticated:
+    context.update({'user_posts_count': posts.filter(poster=request.user).count()})
+
   return render(request, 'index.html', context=context)
 
 
--- members_only_django/settings.py
+++ members_only_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') or ''
+SECRET_KEY = os.getenv('SECRET_KEY') or 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'members_only_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
